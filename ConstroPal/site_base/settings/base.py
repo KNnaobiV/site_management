d12759d5368ac7b1e69aa7e0a4b7ec6ddf0e09b7
knnaobiv <nnaobi.godson@gmail.com>
2026-06-15 06:44:57 +0100

    created vercel json

--- ConstroPal/site_base/settings/base.py
+++ ConstroPal/site_base/settings/base.py
@@ -76,8 +76,8 @@ INSTALLED_APPS = DJANGO_APPS + CUSTOM_USER_APPS + THIRD_PARTY_APPS
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
-    "django.middleware.common.CommonMiddleware",
     "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "allauth.account.middleware.AccountMiddleware",
